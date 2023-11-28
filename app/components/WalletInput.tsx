41c6e6d7e0fab5ed512a8ab7cc93858b7f942563
Owen <owenventer99@gmail.com>
2023-11-28 13:15:29 +0200

    input validation on walletInput

--- app/components/WalletInput.tsx
+++ app/components/WalletInput.tsx
@@ -1,24 +1,37 @@
 "use client";
-import React, { FormEvent, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 
 const WalletInput = () => {
   const searchParams = useSearchParams();
   const [walletAddress, setWalletAddress] = useState("");
+  const [isValid, setIsValid] = useState(true); // State to track validity
   const router = useRouter();
+
+  const validateSolanaPublicKey = (address: string): boolean => {
+    // Simple check: length and allowed characters
+    return /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(address);
+  };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setWalletAddress(e.target.value);
+    setIsValid(true);
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Prevent the default form submission behavior
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const isValidAddress = validateSolanaPublicKey(walletAddress);
+    setIsValid(isValidAddress);
+
+    if (!isValidAddress) {
+      console.log("Invalid Solana public key");
+      return;
+    }
 
     const currentView = searchParams.get("view") || "nfts";
     console.log(currentView);
 
-    // Navigate to the portfolio page with the wallet address and view parameter
     router.push(
       `/portfolio/${encodeURIComponent(walletAddress)}?view=${currentView}`,
     );
@@ -31,11 +44,15 @@ const WalletInput = () => {
         <input
           type="text"
           placeholder="Wallet Address"
-          className="w-full max-w-sm rounded-lg border-2 border-black bg-white text-center text-black focus:border-2 focus:border-black focus:outline-none"
+          className={`h-12 w-full max-w-sm rounded-lg border-2 ${
+            isValid
+              ? "bg-white"
+              : "outline-4 outline-red-500 focus:outline-4 focus:outline-red-500"
+          }  text-center text-black focus:border-2 focus:border-black focus:outline-none`}
           value={walletAddress}
           onChange={handleInputChange}
         />
-        <button
+        {/* <button
           type="submit"
           className="rounded-lg border-2 border-black bg-primary p-1 hover:bg-accent"
         >
@@ -53,7 +70,7 @@ const WalletInput = () => {
               d="M8.25 4.5l7.5 7.5-7.5 7.5"
             />
           </svg>
-        </button>
+        </button> */}
       </div>
     </form>
   );
