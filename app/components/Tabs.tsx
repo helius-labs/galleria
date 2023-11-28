273eab201f0a3bcd3d710c85d7620602eb62f912
Owen <owenventer99@gmail.com>
2023-11-28 22:20:10 +0200

    fixed tabs opacity

--- app/components/Tabs.tsx
+++ app/components/Tabs.tsx
@@ -16,7 +16,7 @@ const Tabs = ({
       <nav className="flex w-full flex-col md:flex-row">
         <Link
           href={`/portfolio/${walletAddress}?view=overview`}
-          className={`block flex-1 rounded bg-black bg-opacity-50 px-4 py-2 text-center font-bold hover:bg-neutral-500 md:inline-flex md:justify-center ${
+          className={`block flex-1 rounded bg-black bg-opacity-50 px-4 py-2 text-center font-bold hover:bg-neutral-500 hover:bg-opacity-50 md:inline-flex md:justify-center ${
             searchParams.view === "overview"
               ? "  border-b-4 border-primary text-primary"
               : "text-white"
@@ -26,7 +26,7 @@ const Tabs = ({
         </Link>
         <Link
           href={`/portfolio/${walletAddress}?view=tokens`}
-          className={`block flex-1 rounded bg-black bg-opacity-50 px-4 py-2 text-center font-bold hover:bg-neutral-500 md:inline-flex md:justify-center ${
+          className={`block flex-1 rounded bg-black bg-opacity-50 px-4 py-2 text-center font-bold hover:bg-neutral-500 hover:bg-opacity-50 md:inline-flex md:justify-center ${
             searchParams.view === "tokens"
               ? "  border-b-4 border-primary text-primary"
               : "text-white"
@@ -36,7 +36,7 @@ const Tabs = ({
         </Link>
         <Link
           href={`/portfolio/${walletAddress}?view=nfts`}
-          className={`block flex-1 rounded bg-black bg-opacity-50 px-4 py-2 text-center font-bold hover:bg-neutral-500 md:inline-flex md:justify-center ${
+          className={`block flex-1 rounded bg-black bg-opacity-50 px-4 py-2 text-center font-bold hover:bg-neutral-500 hover:bg-opacity-50 md:inline-flex md:justify-center ${
             searchParams.view === "nfts"
               ? "  border-b-4 border-primary text-primary"
               : "text-white"
