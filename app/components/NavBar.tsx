55392716d32d97a9b30dafb83b585a9c1316cecc
Nathan Galindo <nathan.galindo@tyson.com>
2023-12-07 21:57:15 +0100

    redesigned navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -14,7 +14,7 @@ const Logo = () => {
       href="/"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className="relative inline-block w-10 sm:w-1/3 cursor-pointer"
+      className="relative inline-block w-10 md:w-1/3 cursor-pointer"
     >
       <picture>
         {/* Mobile logo: shown on screens smaller than 768px */}
@@ -78,74 +78,64 @@ const HeliusForwardLink = ({ className }: { className: string; }) => {
   );
 };
 
-// const MenuDropdown = () => {
-//   return (
-//     <Menu as="div" className="relative ml-3">
-//       <div>
-//         <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-//           <span className="absolute -inset-1.5" />
-//           <span className="sr-only">Open user menu</span>
-//           <img
-//             className="h-8 w-8 rounded-full"
-//             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-//             alt=""
-//           />
-//         </Menu.Button>
-//       </div>
-//       <Transition
-//         as={Fragment}
-//         enter="transition ease-out duration-100"
-//         enterFrom="transform opacity-0 scale-95"
-//         enterTo="transform opacity-100 scale-100"
-//         leave="transition ease-in duration-75"
-//         leaveFrom="transform opacity-100 scale-100"
-//         leaveTo="transform opacity-0 scale-95"
-//       >
-//         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-//           <Menu.Item>
-//             {({ active }) => (
-//               <a
-//                 href="#"
-//                 className={classNames(
-//                   active ? "bg-gray-100" : "",
-//                   "block px-4 py-2 text-sm text-gray-700",
-//                 )}
-//               >
-//                 Your Profile
-//               </a>
-//             )}
-//           </Menu.Item>
-//           <Menu.Item>
-//             {({ active }) => (
-//               <a
-//                 href="#"
-//                 className={classNames(
-//                   active ? "bg-gray-100" : "",
-//                   "block px-4 py-2 text-sm text-gray-700",
-//                 )}
-//               >
-//                 Settings
-//               </a>
-//             )}
-//           </Menu.Item>
-//           <Menu.Item>
-//             {({ active }) => (
-//               <a
-//                 href="#"
-//                 className={classNames(
-//                   active ? "bg-gray-100" : "",
-//                   "block px-4 py-2 text-sm text-gray-700",
-//                 )}
-//               >
-//                 Sign out
-//               </a>
-//             )}
-//           </Menu.Item>
-//         </Menu.Items>
-//       </Transition>
-//     </Menu>
-//   );
-// };
+const MenuDropdown = () => {
+  const menuOptions = [
+    { name: "Tokens", href: "#" },
+    { name: "NFTs", href: "#" },
+  ];
+
+  return (
+    <Menu as="div" className="relative ml-3">
+      <div>
+        <Menu.Button className="relative flex text-sm">
+          <span className="absolute -inset-1.5" />
+          <span className="sr-only">Open dropdown menu</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {menuOptions.map(option => (
+            <Menu.Item key={option.name}>
+              {({ active }) => (
+                <a
+                  href={option.href}
+                  className={classNames(
+                    active ? "bg-gray-100" : "",
+                    "block px-4 py-2 text-sm text-gray-700",
+                  )}
+                >
+                  {option.name}
+                </a>
+              )}
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
 
 const NavBar = () => {
   return (
@@ -157,6 +147,7 @@ const NavBar = () => {
         </div>
 
         {/* <WalletInput source={"navBar"} /> */}
+        <MenuDropdown />
       </nav>
     </>
   );
