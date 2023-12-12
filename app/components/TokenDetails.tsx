1e9d14bbad7274508efdfc767f1021eecb02ac62
Nathan Galindo <nathan.galindo@tyson.com>
2023-12-11 22:09:12 -0600

    format token 22 data

--- app/components/TokenDetails.tsx
+++ app/components/TokenDetails.tsx
@@ -34,18 +34,58 @@ const TokenDetails = ({
 
     const renderValue = (value: any, key: string, indent: number) => {
       if (typeof value === "object" && value !== null) {
-        return (
-          <div key={key} style={{ paddingLeft: `${indent * 20}px` }}>
-            <p className=" text-xl font-bold underline "> {key}:</p>
-            {Object.entries(value).map(([innerKey, innerValue]) =>
-              renderValue(innerValue, innerKey, indent + 1),
-            )}
-          </div>
-        );
+        if (indent === 0) {
+          return (
+            <div className="w-full border-gray-600">
+              <p className="w-40 rounded-md bg-gray-700/60 px-3 py-1.5 text-sm font-medium ring-1 ring-inset ring-white/10">
+                {key}:
+              </p>
+              <div className="mt-2 border-l border-dashed border-gray-600">
+                {Object.entries(value).map(([innerKey, innerValue]) =>
+                  renderValue(innerValue, innerKey, indent + 1),
+                )}
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <div
+              className="mt-4 w-full  border-gray-600"
+              style={{ paddingLeft: `${indent * 20}px` }}
+            >
+              <p className="text-gray-300 w-40 rounded-md bg-gray-700/20 px-3 py-1.5 text-sm font-medium ring-1 ring-inset ring-white/30">
+                {key}:
+              </p>
+              <div className="border-l border-dashed border-gray-600 mt-2">
+                {Object.entries(value).map(([innerKey, innerValue]) =>
+                  renderValue(innerValue, innerKey, indent + 1),
+                )}
+              </div>
+            </div>
+          );
+        }
       } else {
         return (
-          <p className="leading-7" style={{ paddingLeft: `${indent * 20}px` }}>
-            <span className="font-bold ">{key}</span>: {value}
+          <p
+            className="w-full py-1 leading-7 flex items-center overflow-scroll"
+            style={{ paddingLeft: `${indent * 20}px` }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-3 w-3 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+            <span className="font-base text-sm text-gray-400">{key}</span>:
+            <span className="font-normal text-sm ml-1">{` ${value}`}</span>
           </p>
         );
       }
@@ -58,6 +98,8 @@ const TokenDetails = ({
 
   return (
     <div className="h-full w-full overflow-y-auto overflow-x-clip rounded-lg bg-neutral-800 p-2 text-white shadow-glow sm:p-2">
+
+      {/* Header */}
       <div className="relative">
         <Link href={`/portfolio/${walletAddress}?view=${searchParams.view}`}>
           <svg
@@ -78,6 +120,7 @@ const TokenDetails = ({
         <h1 className="m-2 p-3 text-3xl font-bold">{title}</h1>
       </div>
 
+      {/* Body */}
       <div>
         <div className="flex flex-col justify-evenly break-words sm:flex-row">
           <div className="w-full p-3 sm:w-1/2">
@@ -132,6 +175,8 @@ const TokenDetails = ({
             </div>
           </div>
         </div>
+
+        {/* Token 22 Details */}
         <div className="flex justify-center">
           <div className="mx-4 w-full p-3">
             {tokenData[0].mint_extensions && (
