506a5e5cadbf7f2b485b14021c8adaac1c9ca3b1
Nathan Galindo <nathan.galindo@tyson.com>
2023-12-18 17:05:36 -0600

    added dashboard view

--- app/portfolio/[walletAddress]/page.tsx
+++ app/portfolio/[walletAddress]/page.tsx
@@ -1,4 +1,22 @@
-import React, { Suspense } from "react";
+"use client";
+
+import React, { Suspense, Fragment, useState } from "react";
+import { Dialog, Menu, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  BellIcon,
+  CalendarIcon,
+  ChartPieIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/20/solid";
 
 import {
   NavBar,
@@ -9,6 +27,21 @@ import {
   TokenDetails,
 } from "@/app/components";
 import { FungibleToken, NonFungibleToken } from "@/app/types";
+import { classNames } from "@/app/utils";
+
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Team", href: "#", icon: UsersIcon, current: false },
+  { name: "Projects", href: "#", icon: FolderIcon, current: false },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
+  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+];
+
+const userNavigation = [
+  { name: "Your profile", href: "#" },
+  { name: "Sign out", href: "#" },
+];
 
 interface PortfolioPageProps {
   searchParams: { view: string; details: string; tokenDetails: string };
@@ -16,19 +49,278 @@ interface PortfolioPageProps {
 }
 
 const PortfolioPage = async ({ searchParams, params }: PortfolioPageProps) => {
-  // Fetch both fungible and non-fungible token data
-  const { fungibleTokens, nonFungibleTokens } = await getAllAssets(
-    params.walletAddress,
+  const [fungibleTokenData, setFungibleTokenData] = useState<FungibleToken[]>(
+    [],
   );
+  const [nonFungibleTokenData, setNonFungibleTokenData] = useState<
+    NonFungibleToken[]
+  >([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const fungibleData = await getFungibleData(params.walletAddress);
+      const nonFungibleData = await getNonFungibleData(params.walletAddress);
+      setFungibleTokenData(fungibleData);
+      setNonFungibleTokenData(nonFungibleData);
+    };
+    fetchData();
+  }, [params.walletAddress]);
+
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="h-screen bg-radial-gradient">
       <div>
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-50 lg:hidden"
+            onClose={setSidebarOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setSidebarOpen(false)}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                  </Transition.Child>
+
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
+                    <div className="flex h-16 shrink-0 items-center">
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <nav className="flex flex-1 flex-col">
+                      <ul role="list" className="-mx-2 flex-1 space-y-1">
+                        {navigation.map((item) => (
+                          <li key={item.name}>
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                item.current
+                                  ? "bg-gray-800 text-white"
+                                  : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                                "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                              )}
+                            >
+                              <item.icon
+                                className="h-6 w-6 shrink-0"
+                                aria-hidden="true"
+                              />
+                              {item.name}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-50 lg:block lg:w-20 lg:overflow-y-auto lg:bg-gray-900 lg:pb-4">
+          <div className="flex h-16 shrink-0 items-center justify-center">
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Your Company"
+            />
+          </div>
+          <nav className="mt-8">
+            <ul role="list" className="flex flex-col items-center space-y-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-800 text-white"
+                        : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                      "group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-6",
+                    )}
+                  >
+                    <item.icon
+                      className="h-6 w-6 shrink-0"
+                      aria-hidden="true"
+                    />
+                    <span className="sr-only">{item.name}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+
+        <div className="lg:pl-20">
+          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            {/* Separator */}
+            <div
+              className="h-6 w-px bg-gray-900/10 lg:hidden"
+              aria-hidden="true"
+            />
+
+            <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+              <form className="relative flex flex-1" action="#" method="GET">
+                <label htmlFor="search-field" className="sr-only">
+                  Search
+                </label>
+                <MagnifyingGlassIcon
+                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <input
+                  id="search-field"
+                  className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+                  placeholder="Search..."
+                  type="search"
+                  name="search"
+                />
+              </form>
+              <div className="flex items-center gap-x-4 lg:gap-x-6">
+                <button
+                  type="button"
+                  className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Separator */}
+                <div
+                  className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
+                  aria-hidden="true"
+                />
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative">
+                  <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="h-8 w-8 rounded-full bg-gray-50"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                    <span className="hidden lg:flex lg:items-center">
+                      <span
+                        className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                        aria-hidden="true"
+                      >
+                        Tom Cook
+                      </span>
+                      <ChevronDownIcon
+                        className="ml-2 h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Menu.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                      {userNavigation.map((item) => (
+                        <Menu.Item key={item.name}>
+                          {({ active }) => (
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                active ? "bg-gray-50" : "",
+                                "block px-3 py-1 text-sm leading-6 text-gray-900",
+                              )}
+                            >
+                              {item.name}
+                            </a>
+                          )}
+                        </Menu.Item>
+                      ))}
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <main className="xl:pl-96">
+            <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+              {/* Main area */}
+            </div>
+          </main>
+        </div>
+
+        <aside className="fixed bottom-0 left-20 top-16 hidden w-96 overflow-y-auto border-r border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
+          {/* Secondary column (hidden on smaller screens) */}
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+{/* <div>
         {searchParams.details && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-neutral-700 bg-opacity-70">
             <div className="h-4/5 w-10/12 sm:w-2/3">
               <NFTDetails
-                nftData={nonFungibleTokens.filter(
+                nftData={nonFungibleTokenData.filter(
                   (item) => item.id === searchParams.details,
                 )}
                 searchParams={"view=" + searchParams.view}
@@ -43,7 +335,7 @@ const PortfolioPage = async ({ searchParams, params }: PortfolioPageProps) => {
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-neutral-700 bg-opacity-70">
             <div className="h-4/5 w-10/12 sm:w-2/3">
               <TokenDetails
-                tokenData={fungibleTokens.filter(
+                tokenData={fungibleTokenData.filter(
                   (item) => item.id === searchParams.tokenDetails,
                 )}
                 searchParams={searchParams}
@@ -81,32 +373,29 @@ const PortfolioPage = async ({ searchParams, params }: PortfolioPageProps) => {
                 walletAddress={params.walletAddress}
               />
             )} */}
-            {searchParams.view === "tokens" && (
-              <Tokens
-                tokens={fungibleTokens}
-                searchParams={searchParams.toString()}
-                walletAddress={params.walletAddress}
-              />
-            )}
-            {searchParams.view === "nfts" && (
-              <NFTs
-                tokens={nonFungibleTokens}
-                searchParams={searchParams.toString()}
-                walletAddress={params.walletAddress}
-              />
-            )}
-          </div>
-        </Suspense>
-      </div>
-    </div>
-  );
-};
+        //     {searchParams.view === "tokens" && (
+        //       <Tokens
+        //         tokens={fungibleTokenData}
+        //         searchParams={searchParams.toString()}
+        //         walletAddress={params.walletAddress}
+        //       />
+        //     )}
+        //     {searchParams.view === "nfts" && (
+        //       <NFTs
+        //         tokens={nonFungibleTokenData}
+        //         searchParams={searchParams.toString()}
+        //         walletAddress={params.walletAddress}
+        //       />
+        //     )}
+        //   </div>
+        // </Suspense>
+      // </div> */}
 
-const getAllAssets = async (walletAddress: string) => {
+const getFungibleData = async (walletAddress: string) => {
   const url = `https://glori-cpoxlw-fast-mainnet.helius-rpc.com/`;
 
   const response = await fetch(url, {
-    next: { revalidate: 5 },
+    next: { revalidate: 30 },
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -117,11 +406,9 @@ const getAllAssets = async (walletAddress: string) => {
       method: "searchAssets",
       params: {
         ownerAddress: walletAddress,
-        tokenType: "all", // Changed to "all" to fetch both types
+        tokenType: "fungible",
         displayOptions: {
           showNativeBalance: true,
-          showInscription: true,
-          showCollectionMetadata: true,
         },
       },
     }),
@@ -132,17 +419,8 @@ const getAllAssets = async (walletAddress: string) => {
   }
 
   const data = await response.json();
-  const items: (FungibleToken | NonFungibleToken)[] = data.result.items;
 
-  // Split the items into fungible and non-fungible tokens
-  const fungibleTokens: FungibleToken[] = items.filter(
-    (item): item is FungibleToken =>
-      item.interface === "FungibleToken" || item.interface === "FungibleAsset",
-  );
-  const nonFungibleTokens: NonFungibleToken[] = items.filter(
-    (item): item is NonFungibleToken =>
-      !["FungibleToken", "FungibleAsset"].includes(item.interface),
-  );
+  const tokens: FungibleToken[] = data.result.items;
 
   // Calculate SOL balance from lamports
   const solBalance = data.result.nativeBalance.lamports;
@@ -198,7 +476,7 @@ const getAllAssets = async (walletAddress: string) => {
       delegated: false,
       delegate: null,
       ownership_model: "token",
-      owner: nonFungibleTokens[0]?.ownership.owner,
+      owner: tokens[0]?.ownership.owner,
     },
     supply: null, // Assuming null for SOL
     mutable: true, // Assuming true for SOL
@@ -221,10 +499,44 @@ const getAllAssets = async (walletAddress: string) => {
 
   // Add SOL token to the tokens array
   if (solBalance > 0) {
-    fungibleTokens.push(solToken);
+    tokens.push(solToken);
   }
 
-  return { fungibleTokens, nonFungibleTokens };
+  return tokens;
+};
+
+const getNonFungibleData = async (walletAddress: string) => {
+  const url = `https://glori-cpoxlw-fast-mainnet.helius-rpc.com/`;
+
+  const response = await fetch(url, {
+    next: { revalidate: 30 },
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "my-id",
+      method: "searchAssets",
+      params: {
+        ownerAddress: walletAddress,
+        tokenType: "nonFungible",
+        displayOptions: {
+          showInscription: true,
+          showCollectionMetadata: true,
+        },
+      },
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data`);
+  }
+
+  const data = await response.json();
+  const tokens: NonFungibleToken[] = data.result.items;
+
+  return tokens;
 };
 
 export default PortfolioPage;
